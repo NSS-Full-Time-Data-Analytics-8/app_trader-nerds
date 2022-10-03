30945b373a168d97fdefb86cfee6cc3bc4277198
Lewis-Garvin <lewisgarvin@gmail.com>
2022-10-03 07:26:09 -0500

    Updated app store only query to have same columns. Added play store only query.

--- Main_queries.sql
+++ Main_queries.sql
@@ -21,7 +21,7 @@ WITH app_weighted_avg AS
 			 / (a.review_count::numeric + p.review_count) AS weighted_avg_rating
 	FROM app_store_apps a INNER JOIN play_store_apps p USING(name))
 SELECT DISTINCT p.name,
-	            'both' AS stores,
+	            'both' AS store,
 				ROUND(5000 * (12 + 6 * ROUND(wa.weighted_avg_rating / 0.25,0))   --total revenue
 					  - 1000 * (12 + 6 * ROUND(wa.weighted_avg_rating / 0.25,0)) --minus total marketing expense
 					  - CASE WHEN wa.weighted_avg_price <= 2.5 THEN 25000        --minus rights purchase expense
@@ -68,22 +68,31 @@ WHERE category LIKE 'FAMILY';
 --------------------------------
 ----APPS IN APP STORE ONLY------
 
---I plan to update this query so that the result set will have the same fields as the above query for both stores.
---This way, we can combine the results in Excel with a consistent set of columns.
-
 SELECT DISTINCT a.name,
-	   a.price,
-	   ROUND(CASE WHEN a.price <= 2.5 THEN 25000
-             ELSE a.price * 10000 END, 2) AS rights_purchase_cost,
-       a.rating, 
-	   12 + 6 * ROUND(a.rating / 0.25,0)          as lifespan_months,
-	   2500 * (12 + 6 * ROUND(a.rating / 0.25,0)) as total_revenue,
-	   1000 * (12 + 6 * ROUND(a.rating / 0.25,0)) as total_marketing_expense,
-	   ROUND(2500 * (12 + 6 * ROUND(a.rating / 0.25,0))   --total revenue
-		     - 1000 * (12 + 6 * ROUND(a.rating / 0.25,0)) --minus total marketing expense
-		     - CASE WHEN a.price <= 2.5 THEN 25000        --minus rights purchase cost
-			    	ELSE a.price * 10000 END
-	    	 , 2) AS total_profit						  --equals total profit
+				'app_store' AS store,
+				ROUND(2500 * (12 + 6 * ROUND(a.rating / 0.25,0))   --total revenue
+				 	  - 1000 * (12 + 6 * ROUND(a.rating / 0.25,0)) --minus total marketing expense
+					  - CASE WHEN a.price <= 2.5 THEN 25000        --minus rights purchase cost
+							 ELSE a.price * 10000 END
+					  , 2) AS total_profit,						   --equals total profit
+			    2500 * (12 + 6 * ROUND(a.rating / 0.25,0)) AS total_in_app_revenue,
+			    1000 * (12 + 6 * ROUND(a.rating / 0.25,0)) AS total_marketing_expense,
+			    ROUND(CASE WHEN a.price <= 2.5 THEN 25000
+			   		  ELSE a.price * 10000 END, 2)         AS rights_purchase_expense,
+			    12 + 6 * ROUND(a.rating / 0.25,0)          AS lifespan_months,
+				a.rating   AS app_store_rating,
+				NULL       AS play_store_rating,
+				a.rating   AS weighted_avg_rating,
+				a.review_count::numeric AS app_store_review_count,
+				NULL       AS play_store_review_count,
+				a.price    AS app_store_price,
+				NULL       AS play_store_price,
+				a.price    AS weighted_avg_price,
+				a.content_rating AS app_store_content_rating,
+				NULL	   AS play_store_content_rating,
+				a.primary_genre AS app_store_primary_genre,
+				NULL       AS play_store_category,
+				NULL       AS play_store_genres
 FROM app_store_apps a LEFT JOIN play_store_apps p USING(name)
 WHERE p.name IS NULL
 ORDER BY total_profit DESC;
@@ -91,4 +100,32 @@ ORDER BY total_profit DESC;
 ---------------------------------
 ----APPS IN PLAY STORE ONLY------
 
---not yet written
+SELECT DISTINCT p.name,
+				'play_store' AS store,
+				ROUND(2500 * (12 + 6 * ROUND(p.rating / 0.25,0))   --total revenue
+				 	  - 1000 * (12 + 6 * ROUND(p.rating / 0.25,0)) --minus total marketing expense
+					  - CASE WHEN p.price::money::numeric <= 2.5 THEN 25000 --minus rights purchase cost
+							 ELSE p.price::money::numeric * 10000 END
+					  , 2) AS total_profit,						   --equals total profit
+			    2500 * (12 + 6 * ROUND(p.rating / 0.25,0)) AS total_in_app_revenue,
+			    1000 * (12 + 6 * ROUND(p.rating / 0.25,0)) AS total_marketing_expense,
+			    ROUND(CASE WHEN p.price::money::numeric <= 2.5 THEN 25000
+			   		  ELSE p.price::money::numeric * 10000 END, 2) AS rights_purchase_expense,
+			    12 + 6 * ROUND(p.rating / 0.25,0)          AS lifespan_months,
+				NULL       AS app_store_rating,
+				p.rating   AS play_store_rating,
+				p.rating   AS weighted_avg_rating,
+				NULL       AS app_store_review_count,
+				p.review_count AS play_store_review_count,
+				NULL       AS app_store_price,
+				p.price::money::numeric AS play_store_price,
+				p.price::money::numeric AS weighted_avg_price,
+				NULL       AS app_store_content_rating,
+				p.content_rating AS play_store_content_rating,
+				NULL       AS app_store_primary_genre,
+				p.category AS play_store_category,
+				p.genres   AS play_store_genres
+FROM play_store_apps p LEFT JOIN app_store_apps a USING(name)
+WHERE a.name IS NULL
+      AND p.rating IS NOT NULL
+ORDER BY total_profit DESC;
